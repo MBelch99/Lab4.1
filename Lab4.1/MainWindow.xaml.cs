c9a46605ec8b53c32be1de54e73a5f5aa8c29cc2
MBelch99 <84323578+MBelch99@users.noreply.github.com>
2021-06-19 11:38:52 +0200

    Dodaj pliki projektów.

---
+++ Lab4.1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab4._1
+{
+    public List<Student> ListaStudentow { get; set; }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            ListaStudentow = new Lista<Student>()
+                {
+                new Student() { imie="Jan", nazwisko="Kowalski", nrindeksu=1234, wydzial="KIS"},
+                new Student() { imie = "Anna", nazwisko = "Nowak", nrindeksu =4321, wydzial = "KIS" },
+                new Student() { imie = "Michał", nazwisko = "Jarek", nrindeksu =3456, wydzial = "KIS" }
+            };
+            InitializeComponent();
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Imię", Binding = new Binding("imie") });
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Nazwisko", Binding = new Binding("nazwisko") });
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Nr indeksu", Binding = new Binding("nrindeksu") });
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Wydzial", Binding = new Binding("wydzial") });
+
+            dgStudent.AutoGenerateColumns = false;
+            dgStudent.ItemsSource = ListaStudentów;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab4.1/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4._1
+{
+    public class Student
+    {      
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public int nrindeksu { get; set; }
+        public string wydzial { get; set; }
+
+
+        public Student(string imie, string nazwisko,int nrindeksu, string wydzial)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.nrindeksu = nrindeksu;
+            this.wydzial = wydzial;
+        }
+
+        public Student()
+            :this("","",0,"")
+        { }
+    }
+}
